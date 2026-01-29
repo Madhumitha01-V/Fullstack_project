f729f4ff295adf6f6421b77fd5d7710a8eb75adb
Madhumitha01-V <madhu61104@gmail.com>
2026-01-29 14:51:55 +0530

    Initial fullstack project

---
+++ Javascript/demo.js
@@ -0,0 +1,165 @@
+// var a = 10
+// var b = 20
+// console.log(a+b)
+
+
+// function samsung()
+// {
+//     console.log("This is Samsung Phone")
+// }
+
+// function oppo()
+// {
+//     console.log("This is Oppo Phone")
+// }
+
+// function redmi()
+// {
+//     console.log("This is Redmi Phone")
+// }
+
+// samsung()
+// oppo()
+// redmi()
+
+// function add()
+// {
+//     console.log(a+b)
+// }
+
+// var a = 10
+// var b = 20
+// add()
+
+// var factor = "Rajinikanth"
+// var fplayer = "Virat Kohli"
+// var fmovie = "Paddikathavan"
+
+// function favourite()
+// {
+//     console.log("Favourite Actor:",factor)
+//     console.log("Favourite Player:",fplayer)
+//     console.log("Favourite Movie:",fmovie)
+// }
+
+// favourite()
+
+// function area(l,b)
+// {
+//     console.log("Area is:"+l*b)
+// }
+// var length = 15
+// var breadth = 25
+
+// area(15,25)
+
+// function add(a,b)
+// {
+//     return a+b
+// }
+
+// var  tot = add(10,20)
+// console.log(tot)
+
+// var rain = false
+// if(rain)
+// {
+//     console.log("Take an umbrella")
+// }
+// else
+// {
+//     console.log("Enjoy the sunshine")
+// }
+
+// var hw = true
+// if(hw){
+//     console.log("Great Job")
+// }
+// else{
+//     console.log("Finish your homework befor playing")
+// }
+
+// var cookies = true
+// if(cookies){
+//     console.log("Would you like a cookie?")
+// }
+// else{
+//     console.log("Time to bake more cookies")
+// }
+
+// var brush = true
+// if(brush){
+//     console.log("Your teeth are clean")
+// }
+// else{
+//     console.log("Go brush your teeth")
+// }
+
+// var bday = true
+// if(bday){
+//     console.log("Happy Birthday!Enjoy your special day")
+// }
+// else{
+//     console.log("Have a great day")
+// }
+
+// 
+
+// var season = "winter"
+// if(season == "spring")
+// {
+//     console.log("Enjoy the blooming flowers")
+// }
+// if(season == "summer")
+// {
+//     console.log("Have fun in the sun")
+// }
+// if(season == "autumn"||season =="fall")
+// {
+//     console.log("Admire the colorful leaves")
+// }
+// if(season=="winter")
+// {
+//     console.log("Bundle up and stay warm")
+// }
+
+// var score = 150
+// if(score<=50)
+// {
+//     console.log("You need to improve")
+// }
+// else if(score>50 && score<=70)
+// {
+//     console.log("Good Job!")
+// }
+// else if(score>70 && score<=100)
+// {
+//     console.log("Excellent performace!")
+// }
+// else{
+//     console.log("Invalid number")
+// }
+
+// var num = 111
+// if(num%2==0)
+// {
+//     console.log("The number is even")
+// }
+// else
+// {
+//     console.log("The number is odd")
+// }
+
+// var char = "t"
+// if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u")
+// {
+//     console.log("Its vowel!")
+// }
+// else{
+//     console.log("Its a consonant")
+// }
+
+for(i=1;i<=10;i++)
+{
+    console.log(i+" x 2 = "+i*2)
+}
---
+++ Javascript/todo.js
@@ -0,0 +1,5 @@
+var fruit = ["apple","orange","mango"]
+for(i=0;i<3;i++)
+{
+    console.log(fruit[i])
+}
